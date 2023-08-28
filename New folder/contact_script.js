4df1bc42f616ee5686d2aab83cc6db39dc27e74c
Shubham Singh <119438226+Shubham5925@users.noreply.github.com>
2023-08-28 17:06:20 +0530

    Add files via upload

---
+++ New folder/contact_script.js
@@ -0,0 +1,8 @@
+function open_contact()
+{
+	document.getElementById("mydetails").style.display="block";
+}
+function close_contact()
+{
+	document.getElementById("mydetails").style.display="none";
+}
